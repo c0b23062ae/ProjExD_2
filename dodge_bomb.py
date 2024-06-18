c8714e14d78a89209ca7e3d6895670beae61b537
sosuu <c0b23062ae@edu.teu.ac.jp>
2024-06-18 14:19:48 +0900

    練習２：爆弾Surfaceの作成と移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -20,8 +21,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    bb_img = pg.Surface((20, 20))   # 1辺が20の空のSurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)   # 空のSurfaceに赤い円を描く
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx = +5
+    vy = +5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -36,6 +45,8 @@ def main():
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)          # どのくらい現在地から動くか
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)   
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
